9643ce93e58bd0febaf36898a96a64992548fc35
Joans <joans@example.com>
2026-05-25 06:37:14 +0700

    FIX PORT

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
-import tailwindcss from '@tailwindcss/vite';  // ganti ini
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: node({
+    mode: 'standalone'
+  }),
   vite: {
-    plugins: [tailwindcss()],  // pindah ke vite.plugins
+    plugins: [tailwindcss()],
   },
-  server: {
-    host: true,
-    port: Number(process.env.PORT ?? 3000)
-  }
 });
